a40178ba8b7853454b931882bbfe65a2a33a733d
yuni <RimsEther358@outlook.jp>
2024-04-27 00:51:32 +0900

    Update SetTemplateReferenceLayer.lua

--- Scripts/SetTemplateReferenceLayer.lua
+++ Scripts/SetTemplateReferenceLayer.lua
@@ -1,1 +1,17 @@
 -- reference layer should be separated cause parts have different zIndex.
+
+--[[
+useful commands
+
+https://www.aseprite.org/docs/cli/#export-all-layers-into-different-png/gif-files
+--import-layer <name>    Include just the given layer in the sheet
+--all-layers             Make all layers visible
+
+--]]
+
+--[[
+ an idea.
+
+ i wana import a parts layer that has cell data it has already linked each other from list select menue
+
+ --]]
